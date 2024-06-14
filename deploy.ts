3b12341ef17157ada3e20e58455d756929b616bb
Joshua Hemphill <j1.robofan@gmail.com>
2024-06-13 20:38:41 -0700

    change to gitea

--- deploy.ts
+++ deploy.ts
@@ -1,3 +1,3 @@
 #!/usr/bin/env -S deno run --allow-all
-import "./lib/deploy-podman.ts";
-import "./lib/deploy-caddy.ts";
+await import("./lib/deploy-podman.ts");
+await import("./lib/deploy-caddy.ts");
--- lib/deploy-podman.ts
+++ lib/deploy-podman.ts
@@ -1,5 +1,20 @@
 #!/usr/bin/env -S deno run --allow-all
 import { $ } from "jsr:@david/dax";
+import * as log from "jsr:@std/log";
+import { parseAll as parseYaml } from "jsr:@std/yaml";
 
-$`podman kube play --replace --start=false \
+await $`podman kube play --replace --no-hosts --userns=keep-id --start=false \
 --annotation hosting=services ./.bin/services.kube.yaml`;
+
+log.info(
+  "Podman kube play successfully build out all pods/containers/volumes!",
+);
+const confirmStart = confirm("Start all services?");
+if (confirmStart) {
+  const deploymentYaml = await Deno.readTextFile(".bin/services.kube.yaml");
+  const deployment = parseYaml(deploymentYaml) as TemplateKubeYaml[];
+  for (const pod of deployment) {
+    log.info(`Starting pod ${pod.metadata.name}...`);
+    await $`podman pod start ${pod.metadata.name}`;
+  }
+}
